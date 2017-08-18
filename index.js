9ea0cfdad515429da7b04a8c172033d76012e9c0
Jonas Eckerström <jonaseck@gmail.com>
2017-08-18 22:33:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var gpio = require('node-gpio');
+var GPIO = gpio.GPIO;
+var mqtt = require('mqtt')
+
+var config = {
+    'mqtt': {},
+    'button': {}
+}
+
+config.mqtt.broker = process.env.MQTT_BROKER || 'localhost'
+config.mqtt.port = process.env.MQTT_PORT || '1883'
+config.mqtt.prefix = process.env.MQTT_PREFIX || 'media'
+config.mqtt.topic = process.env.MQTT_TOPIC || 'button'
+config.mqtt.user = process.env.MQTT_USER
+config.mqtt.password = process.env.MQTT_PASSWORD
+config.button.channel = process.env.BUTTON_CHANNEL || '17'
+
+var options = {}
+if(config.mqtt.user) options.username = config.mqtt.user
+if(config.mqtt.password) options.password = config.mqtt.password
+
+var client  = mqtt.connect('mqtt://' + config.mqtt.broker + ':' + config.mqtt.port, options)
+client.on('connect', function () {
+  console.log("MQTT Connected")
+});
+
+var button = new GPIO(config.button.channel);
+button.open();
+button.setMode(gpio.IN);
+button.write(gpio.LOW);
+
+button.on("changed", function (value) {
+  if(!value)
+  client.publish(config.mqtt.prefix + '/' + config.mqtt.topic, 'pushed')
+});
+button.listen();
+
+process.on('SIGINT', function () {
+    button.close();
+    client.end()
+    process.exit();
+})
